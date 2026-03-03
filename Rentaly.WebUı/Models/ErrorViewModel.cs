ecc143f8ab7d2a73e7256a05f6e1657cdd5af5cc
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2026-03-03 15:43:59 +0300

    1.ders sonu

---
+++ Rentaly.WebUı/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rentaly.WebUı.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
