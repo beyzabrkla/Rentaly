27ae9a85382780a22f8c1e71ed60a82ef36bf858
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2026-03-05 15:18:47 +0300

    data access katmanı bitti

---
+++ Rentaly.DataAccessLayer/Concrete/RentalyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rentaly.EntityLayer.Entities;
+
+namespace Rentaly.DataAccessLayer.Concrete
+{
+    public class RentalyContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=BEYZA\\BEYZA_DEV;Database=RentalyDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarModel> CarModels { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+    }
+}
