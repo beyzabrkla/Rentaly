5b132317d4847901d5ea111ffaa6a9d902e76673
Beyza Rabia Kılıçaslan <beyzarabia121@gmail.com>
2026-03-11 20:23:58 +0300

    ana sayfa componentler tamamlandı

---
+++ Rentaly.WebUı/ViewComponents/_HomeAdventureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Rentaly.WebUI.ViewComponents
+{
+    public class _HomeAdventureComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
